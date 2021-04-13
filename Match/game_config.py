a01f30ff8af86deb008793a3f52d32b159f922b0
Karthikeyan <58913393+karthikeyan0312@users.noreply.github.com>
2021-04-13 13:57:00 +0530

    Update game_config.py

--- Match/game_config.py
+++ Match/game_config.py
@@ -6,7 +6,7 @@ NUM_TILES_SIDE=4
 NUM_TILES_TOTAL=16
 MARGIN=4
 
-ASSET_DIR=  r'E:\my python\game\New folder\assests'
+ASSET_DIR=  r'...\assests'
 ASSET_FILES=[x for x in os.listdir(ASSET_DIR) if x[-3:].lower()=='png']
 try:
     assert len(ASSET_FILES)==8
